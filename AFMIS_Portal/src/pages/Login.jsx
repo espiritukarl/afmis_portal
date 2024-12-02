d0afaa53431a82989857e602fa5036b7165d5239
espiritukarl <karlespiritukarl@gmail.com>
2024-12-02 16:56:24 +0800

    Finished login modal

--- AFMIS_Portal/src/pages/Login.jsx
+++ AFMIS_Portal/src/pages/Login.jsx
@@ -1,4 +1,5 @@
 import Modal from "../components/Modal";
+import { Link } from "react-router-dom";
 
 export default function Login({ isModalOpen, onClose }) {
   return (
@@ -10,7 +11,26 @@ export default function Login({ isModalOpen, onClose }) {
           <div className="left-half">
             <img src="/login1.jpg" />
           </div>
-          <div className="right-half">hi</div>
+          <div className="right-half">
+            <img src="/dalogo2.png" alt="" />
+            <h2 className="poppins-light">AFMIS PORTAL</h2>
+            <form action="/" className="login-form">
+              <input
+                type="text"
+                placeholder="Username"
+                className="roboto-light"
+              />
+              <input
+                type="text"
+                placeholder="Password"
+                className="roboto-light"
+              />
+              <button className="login-submit">Submit</button>
+            </form>
+            <div className="open-registration roboto-regular">
+              Create an account
+            </div>
+          </div>
         </>
       }
     />
