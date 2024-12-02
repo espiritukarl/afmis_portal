11e9adb8b0165e7e47ee912ed3ba385199334c9d
espiritukarl <karlespiritukarl@gmail.com>
2024-12-02 16:27:55 +0800

    Adding photo for modal - to fix, still need to crop at 50% for img

--- AFMIS_Portal/src/components/Modal.jsx
+++ AFMIS_Portal/src/components/Modal.jsx
@@ -1,11 +1,20 @@
 import "../styles/Modal.css";
+import { Icon } from "@iconify/react/dist/iconify.js";
 
 export default function Modal({ isOpen, onClose, content }) {
   if (!isOpen) return null;
 
   return (
     <div onClick={onClose} className="modal-overlay">
-      <div className="modal-content">{content}</div>
+      <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+        <Icon
+          icon={"line-md:close"}
+          className="modal-close-button"
+          onClick={onClose}
+          width={24}
+        />
+        <div>{content}</div>
+      </div>
     </div>
   );
 }
--- AFMIS_Portal/src/pages/Login.jsx
+++ AFMIS_Portal/src/pages/Login.jsx
@@ -1,5 +1,18 @@
 import Modal from "../components/Modal";
 
 export default function Login({ isModalOpen, onClose }) {
-  return <Modal isOpen={isModalOpen} onClose={onClose} content={<>tite</>} />;
+  return (
+    <Modal
+      isOpen={isModalOpen}
+      onClose={onClose}
+      content={
+        <>
+          <div className="left-half">
+            <img src="/login1.jpg" />
+          </div>
+          <div className="right-half">hi</div>
+        </>
+      }
+    />
+  );
 }
