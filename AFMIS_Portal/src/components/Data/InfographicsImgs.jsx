9e74cade90e7b82e18dd80c53598159e322b4917
espiritukarl <karlespiritukarl@gmail.com>
2024-12-11 10:10:19 +0800

    Initial addition for Infographics page

---
+++ AFMIS_Portal/src/components/Data/InfographicsImgs.jsx
@@ -0,0 +1,11 @@
+export const InfographicImgs = [
+  <img src="/infographics1.jpg" alt="" />,
+  <img src="/infographics2.jpg" alt="" />,
+  <img src="/infographics3.jpg" alt="" />,
+  <img src="/infographics4.jpg" alt="" />,
+  <img src="/infographics5.jpg" alt="" />,
+  <img src="/infographics6.jpg" alt="" />,
+  <img src="/infographics7.jpg" alt="" />,
+  <img src="/infographics8.jpg" alt="" />,
+  <img src="/infographics9.jpg" alt="" />,
+];
