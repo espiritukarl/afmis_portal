865cdd25d3976aba7bc88bffd4f803b9b8ee486c
espiritukarl <karlespiritukarl@gmail.com>
2025-01-13 09:41:31 +0800

    Renamed "AFMIS Portal" to "Agriculture and Fisheries Market Information System"

--- AFMIS_Portal/src/pages/Header.jsx
+++ AFMIS_Portal/src/pages/Header.jsx
@@ -4,7 +4,9 @@ import "../styles/header.css";
 export default function Header() {
   return (
     <header>
-      <h1 className="poppins-medium">AFMIS Portal</h1>
+      <h1 className="poppins-medium">
+        Agriculture and Fisheries Market Information System
+      </h1>
       <a href="https://da.gov.ph/" target="_blank">
         <img src={logo} className="header-logo" alt="Header Logo" />
       </a>
