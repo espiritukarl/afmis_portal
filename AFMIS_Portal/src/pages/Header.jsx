146293e57ab1f387b75452f5654c83ac01ae0f7f
espiritukarl <karlespiritukarl@gmail.com>
2025-01-30 14:53:39 +0800

    Gave a bit more styling to header + h1 is now a clickable link

--- AFMIS_Portal/src/pages/Header.jsx
+++ AFMIS_Portal/src/pages/Header.jsx
@@ -1,11 +1,14 @@
 import logo from "/Header-Logo.svg";
+import { Link } from "react-router-dom";
 import "../styles/header.css";
 
 export default function Header() {
   return (
     <header>
       <h1 className="poppins-medium">
-        Agriculture and Fisheries Market Information System
+        <Link to={"/"}>
+          Agriculture and Fisheries Market Information System
+        </Link>
       </h1>
       <a href="https://da.gov.ph/" target="_blank">
         <img src={logo} className="header-logo" alt="Header Logo" />
