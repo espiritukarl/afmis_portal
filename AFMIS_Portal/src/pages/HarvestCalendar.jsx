8d86b4517816159efedfd24365e5c8662c3d06fa
espiritukarl <karlespiritukarl@gmail.com>
2025-02-05 10:07:19 +0800

    Fully functioning harvest calendar with styling

--- AFMIS_Portal/src/pages/HarvestCalendar.jsx
+++ AFMIS_Portal/src/pages/HarvestCalendar.jsx
@@ -1,18 +1,69 @@
 import { Calendar, globalizeLocalizer, Views } from "react-big-calendar";
 import "react-big-calendar/lib/css/react-big-calendar.css";
+import "../styles/harvestcalendar.css";
 import globalize from "globalize";
 
 const localizer = globalizeLocalizer(globalize);
 
+const events = [
+  //NOTE: Month - 0 to 11 (January to December)
+  //DAY: Ends -1 day (12 means 11 at the calendar)
+  {
+    id: 1,
+    title: "Cabbage Harvest",
+    start: new Date(2025, 1, 6),
+    end: new Date(2025, 1, 12),
+    allDay: true,
+    description: "Cabbage",
+    resourceId: 1,
+    backgroundColor: "#008000",
+    color: "#fff",
+  },
+  {
+    id: 2,
+    title: "Carrot Harvest",
+    start: new Date(2025, 1, 9),
+    end: new Date(2025, 1, 29),
+    allDay: true,
+    description: "Carrot",
+    resourceId: 1,
+    backgroundColor: "#e9692c",
+  },
+  {
+    id: 3,
+    title: "Onion Harvest",
+    start: new Date(2025, 1, 22),
+    end: new Date(2025, 2, 10),
+    allDay: true,
+    description: "Onion",
+    resourceId: 1,
+    backgroundColor: "#62121b",
+    color: "white",
+  },
+];
+
 export default function HarvestCalendar() {
+  const eventStyleGetter = (event) => {
+    return {
+      style: {
+        backgroundColor: event.backgroundColor || "gray",
+        color: event.color || "black",
+        borderRadius: "5px",
+        padding: "5px",
+      },
+    };
+  };
+
   return (
-    <div className="roboto-regular">
+    <div className="lato-regular harvest-calendar">
       <Calendar
         localizer={localizer}
-        defaultView={Views.MONTH}
         startAccessor="start"
         endAccessor="end"
-        style={{ height: 500 }}
+        style={{ height: "54vh", width: "45vw" }}
+        views={["month", "agenda"]}
+        events={events}
+        eventPropGetter={eventStyleGetter}
       />
     </div>
   );
