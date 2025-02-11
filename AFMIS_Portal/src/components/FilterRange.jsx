2e3e51adca958dac4a08cf444eb8477e5c4ce2fd
espiritukarl <karlespiritukarl@gmail.com>
2025-02-11 12:58:39 +0800

    Initial commit of a Filter Range for monthly time period

--- AFMIS_Portal/src/components/FilterRange.jsx
+++ AFMIS_Portal/src/components/FilterRange.jsx
@@ -1,6 +1,6 @@
 import { timePeriod } from "./Data/HomeData";
 
-export function FilterRange({ yearRange, handleYearRangeChange }) {
+export function FilterYearRange({ yearRange, handleYearRangeChange }) {
   return (
     <div className="year-range">
       <div className="range">
@@ -23,6 +23,59 @@ export function FilterRange({ yearRange, handleYearRangeChange }) {
             type="number"
             value={yearRange.end}
             onChange={(e) => handleYearRangeChange(e, "end")}
+            min={timePeriod["Yearly"][0]}
+            max={timePeriod["Yearly"][timePeriod["Yearly"].length - 1]}
+            className="roboto-regular"
+          />
+        </label>
+      </div>
+    </div>
+  );
+}
+
+export function FilterMonthRange({ monthRange, handleMonthRangeChange }) {
+  return (
+    <div className="year-range">
+      <div className="range">
+        <label>
+          <span className="range-label">Start:</span>
+          <select
+            onChange={(e) => handleMonthRangeChange(e, "start", "month")}
+            value={monthRange.start.month}
+          >
+            {timePeriod["Monthly"].map((month, index) => (
+              <option key={month} value={index}>
+                {month}
+              </option>
+            ))}
+          </select>
+          <input
+            type="number"
+            value={monthRange.start.year}
+            onChange={(e) => handleMonthRangeChange(e, "start", "year")}
+            min={timePeriod["Yearly"][0]}
+            max={timePeriod["Yearly"][timePeriod["Yearly"].length - 1]}
+            className="roboto-regular"
+          />
+        </label>
+      </div>
+      <div className="range">
+        <label>
+          <span className="range-label">End:</span>
+          <select
+            onChange={(e) => handleMonthRangeChange(e, "end", "month")}
+            value={monthRange.end.month}
+          >
+            {timePeriod["Monthly"].map((month, index) => (
+              <option key={month} value={index}>
+                {month}
+              </option>
+            ))}
+          </select>
+          <input
+            type="number"
+            value={monthRange.end.year}
+            onChange={(e) => handleMonthRangeChange(e, "end", "year")}
             min={timePeriod["Yearly"][0]}
             max={timePeriod["Yearly"][timePeriod["Yearly"].length - 1]}
             className="roboto-regular"
