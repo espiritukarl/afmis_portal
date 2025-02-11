e0c005e5e502c4a611f0d41bd962de26e12cdb50
espiritukarl <karlespiritukarl@gmail.com>
2025-02-11 12:58:12 +0800

    Moved functions into where it is being used, instead of from the parent component

--- AFMIS_Portal/src/components/FilterPopup.jsx
+++ AFMIS_Portal/src/components/FilterPopup.jsx
@@ -1,16 +1,73 @@
 import { priceTypes, riceCommodity, timePeriod } from "./Data/HomeData";
-import { FilterRange } from "./FilterRange";
+import { FilterYearRange, FilterMonthRange } from "./FilterRange";
 
 export default function FilterPopup({
   filters,
   isOpen,
   yearRange,
-  handleYearRangeChange,
-  handleRiceCheckbox,
-  handlePriceTypeCheckbox,
-  handleRadioChange,
+  monthRange,
+  setYearRange,
+  setMonthRange,
+  setFilterOptions,
 }) {
   if (!isOpen) return null;
+
+  const handleMonthRangeChange = (e, type, time) => {
+    const value = parseInt(e.target.value);
+    setMonthRange((prev) => ({
+      ...prev,
+      [type]: {
+        ...prev[type],
+        [time]: value,
+      },
+    }));
+  };
+
+  const handleYearRangeChange = (e, type) => {
+    const value = parseInt(e.target.value);
+    setYearRange((prev) => ({
+      ...prev,
+      [type]: value,
+    }));
+  };
+
+  const handleRadioChange = (e) => {
+    const { name, value } = e.target;
+    setFilterOptions((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleRiceCheckbox = (e, category) => {
+    const { value, checked } = e.target;
+    setFilterOptions((prev) => {
+      const riceFilters = [...prev.rice];
+      const categoryIndex = riceFilters.findIndex(
+        (item) => item.category === category
+      );
+
+      if (categoryIndex !== -1) {
+        const selected = new Set(riceFilters[categoryIndex].selected);
+        checked ? selected.add(value) : selected.delete(value);
+        riceFilters[categoryIndex].selected = [...selected];
+      } else if (checked) {
+        riceFilters.push({ category, selected: [value] });
+      }
+
+      return {
+        ...prev,
+        rice: riceFilters.filter((item) => item.selected.length > 0),
+      };
+    });
+  };
+
+  const handlePriceTypeCheckbox = (e) => {
+    const { value, checked } = e.target;
+    setFilterOptions((prev) => ({
+      ...prev,
+      priceTypes: checked
+        ? [...prev.priceTypes, value]
+        : prev.priceTypes.filter((type) => type !== value),
+    }));
+  };
 
   return (
     <div className="filter-overlay" onClick={(e) => e.stopPropagation()}>
@@ -18,8 +75,8 @@ export default function FilterPopup({
         <legend>Time Period</legend>
         <div className="filter-options">
           {Object.keys(timePeriod).map((key) => (
-            <>
-              <label key={key} className="roboto-regular">
+            <div key={key}>
+              <label className="roboto-regular">
                 <input
                   type="radio"
                   name="timePeriod"
@@ -27,15 +84,21 @@ export default function FilterPopup({
                   checked={filters.timePeriod === key}
                   onChange={handleRadioChange}
                 />
-                <div>{key.charAt(0).toUpperCase() + key.slice(1)}</div>
+                <span>{key.charAt(0).toUpperCase() + key.slice(1)}</span>
               </label>
               {filters.timePeriod === "Yearly" && key === "Yearly" && (
-                <FilterRange
+                <FilterYearRange
                   yearRange={yearRange}
                   handleYearRangeChange={handleYearRangeChange}
                 />
               )}
-            </>
+              {filters.timePeriod === "Monthly" && key === "Monthly" && (
+                <FilterMonthRange
+                  monthRange={monthRange}
+                  handleMonthRangeChange={handleMonthRangeChange}
+                />
+              )}
+            </div>
           ))}
         </div>
       </fieldset>
