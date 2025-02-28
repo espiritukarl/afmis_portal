4eada15bf38aea919e56bb5dad5b01aad24e6cf2
espiritukarl <karlespiritukarl@gmail.com>
2025-02-28 10:29:00 +0800

    Added way to display agribusiness players from an endpoint

--- AFMIS_Portal/src/pages/AgribusinessDirectory/AgribusinessDirectory.jsx
+++ AFMIS_Portal/src/pages/AgribusinessDirectory/AgribusinessDirectory.jsx
@@ -2,12 +2,13 @@ import { useState, useEffect } from "react";
 import SectionTitle from "../../components/SectionTitle";
 
 export default function AgribusinessDirectory() {
-  const [data, setData] = useState();
+  const [companies, setCompanies] = useState();
+  const [products, setProducts] = useState();
 
   useEffect(() => {
     fetch("http://localhost:3000/agribusiness-companies/")
       .then((res) => res.json())
-      .then((data) => setData(data))
+      .then((data) => setCompanies(data))
       .catch((err) => console.error("Fetched an error: ", err));
   });
 
@@ -15,14 +16,16 @@ export default function AgribusinessDirectory() {
     <main>
       <SectionTitle title={"Directory of Agribusiness Players"} />
       <ul>
-        {data?.map((company) => (
+        {companies?.map((company) => (
           <li key={company.id}>
             <ul>
-              <li>
-                {company.company_name} -{" "}
-                {`${company.street_no} ${company.street_name}, Brgy. ${
-                  company.barangay
-                }, ${company.city}, ${company.province ?? company.region}`}
+              <li className="roboto-light">
+                {company.company_name} {" : "}
+                {`${company.street_no} ${company.street_name} ${
+                  company.barangay ?? ""
+                } ${company.city} ${company.province ?? ""} ${
+                  company.region ?? ""
+                } ${company.zip_code}`}
               </li>
               <li>...</li>
             </ul>
